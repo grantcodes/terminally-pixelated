1a494491cd2bfa5323c532c53acd6164462b4924
Grant Richmond <accounts@terminalpixel.co.uk>
2014-10-08 00:28:13 +0100

    remove ie8 support

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function( grunt ) {
 
     autoprefixer: {
       options: {
-        browsers: ['last 2 version', 'ie 8', 'ie 9']
+        browsers: ['last 2 version']
       },
       dist: {
         src: '*.css',
