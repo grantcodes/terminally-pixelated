86f4e353a7f348529108c5410d55885e34468c2a
Grant Richmond <accounts@terminalpixel.co.uk>
2014-12-29 17:34:08 +0000

    improve pagination and styleguide

--- template-styleguide.php
+++ template-styleguide.php
@@ -5,7 +5,7 @@ $context['post'] = Timber::get_post();
 $context['main_sidebar'] = false;
 $context['styleguide'] = file_get_contents( TPHelpers::get_theme_resource_uri( 'docs/styleguide/index.html' ) );
 add_action( 'wp_enqueue_scripts', function() {
-    wp_enqueue_style( 'highlight', '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/styles/default.min.css' );
+    wp_enqueue_style( 'highlight', '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/styles/tomorrow-night-bright.min.css' );
     wp_enqueue_script( 'highlight', '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/highlight.min.js', array( 'jquery' ), false, true );
     wp_enqueue_script( 'kss', TPHelpers::get_theme_resource_uri( 'docs/styleguide/public/kss.js' ), array( 'jquery', 'highlight' ), false, true );
 } );
